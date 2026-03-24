a9d13d5c3c4b8d33e11635bb68f2851ddd60bae2
nrfx <zavalin001@ya.ru>
2026-03-24 22:24:55 +0800

    сделал 3 страницы по гайду

--- index.php
+++ index.php
@@ -5,9 +5,47 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"  rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-    <div>Всем привет</div>
-    <div>Версия PHP: <?php print phpversion(); ?></div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="/"><i class="fas fa-car"></i></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Главная</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/honda">Honda</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="/toyota">Toyota</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
+    <div class="container">
+    <?php 
+    $url = $_SERVER["REQUEST_URI"];
+
+    echo "Вы на странице: $url, будьте внимательны!<br>";
+
+    if ($url == "/") {
+        require "views/main.php";
+    } elseif ($url == "/toyota") {
+        require "views/toyota.php";
+    } elseif ($url == "/honda") {
+        require "views/honda.php";
+    } 
+    ?>
+    </div>
+    
 </body>
 </html>
---
+++ picture.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- … -->
+</head>
+<body>
+    Я новая страничка!
+    <br>
+    <img src="/images/horse.jpg" alt="">
+</body>
+</html>
