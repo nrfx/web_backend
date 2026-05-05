1d364a2e5518e7f85e0eec374df2f3082003a66f
nrfx <zavalin001@ya.ru>
2026-05-05 11:37:18 +0800

    сделал 4 задание: фильтр по описанию, список типов, бутстрап обертка

--- public/controllers/SearchController.php
+++ public/controllers/SearchController.php
@@ -11,17 +11,20 @@ class SearchController extends BaseCarTwigController
 
         $type = isset($_GET['type']) ? $_GET['type'] : '';
         $title = isset($_GET['title']) ? $_GET['title'] : '';
+        $info = isset($_GET['info']) ? $_GET['info'] : '';
 
         $sql = <<<EOL
 SELECT id, title
 FROM car_objects
-WHERE (:title = '' OR title like CONCAT('%', :title, '%'))
-    AND (type = :type)
+WHERE (:title = '' OR title like CONCAT('%', :title, '%')) 
+    AND (:info = '' OR info like CONCAT('%', :info, '%'))
+    AND (:type = '' OR type = :type)
 EOL;
         $query = $this->pdo->prepare($sql);
 
         $query->bindValue(':title', $title);
         $query->bindValue("type", $type);
+        $query->bindValue("info", $info);
         $query->execute();
 
         $context['objects'] = $query->fetchAll();
