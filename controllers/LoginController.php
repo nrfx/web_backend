811a75b64438c19f8330d956b7fb60b6e41ac77e
nrfx <zavalin001@ya.ru>
2026-05-14 15:29:06 +0800

    подвязал вход на сайт к бд. все готово теперь

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -12,7 +12,14 @@ class LoginController extends TwigBaseController
         $login = $_POST['login'];
         $password = $_POST['password'];
 
-        if ($login === 'admin' && $password === 'admin') {
+        // проверяем по таблице users в БД
+        $query = $this->pdo->prepare("SELECT * FROM users WHERE username = :username AND password = :password");
+        $query->bindValue("username", $login);
+        $query->bindValue("password", $password);
+        $query->execute();
+        $user = $query->fetch();
+
+        if ($user) {
             $_SESSION['is_logged'] = true;
             header('Location: /');
             exit;
